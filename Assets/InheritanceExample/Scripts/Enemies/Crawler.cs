654bd93876caa3f285291be51f91008225566e4b
Eila <eilaboldt01@gmail.com>
2024-01-20 16:11:45 -0600

    Added Charger Enemy
    Included comments, override OnHit() to increase charger move speed every time its hit.

--- Assets/InheritanceExample/Scripts/Enemies/Crawler.cs
+++ Assets/InheritanceExample/Scripts/Enemies/Crawler.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 
 public class Crawler : EnemyBase
 {
+    // replace abstract w/ override to define the method
     protected override void OnHit()
     {
         
     }
+    /*
+    protected override void Move()
+    {
+        // Change movement 
+    }*/
 
     public override void Kill()
     {
         //TODO put code you want to happen before disable here
 
         // this runs the base method AND what's above it here
-        base.Kill();
+        base.Kill(); // w/out base.Kill(), the base method will be override and do nothing
     }
 }
--- Assets/InheritanceExample/Scripts/Enemies/EnemyBase.cs
+++ Assets/InheritanceExample/Scripts/Enemies/EnemyBase.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+
+// MUST add abstract keyword to any class that contains an abstract method
 public abstract class EnemyBase : MonoBehaviour
 {
+    /*
+     * Inheritance:
+     * (Base Class)
+     *  - Virtual: ALLOW this method to be overriden
+     *  - Abstract: Require this method to be overridden, and defined in the inheriting class.
+     *  (Derived Class)
+     *  - Override: Add functionalityto either a virtual or abstract method defined in the parent class.
+     */
+    /*
+     * ANY class can modify health if its public
+     * publc int Health = 3;
+     */
+    // Only this class can modify health
     [SerializeField] private int _health = 3;
     [Header("FX")]
     [SerializeField] private AudioClip _deathSound;
     [SerializeField] private AudioClip _hitSound;
-    [SerializeField] protected float MoveSpeed = .05f;
+    [SerializeField] protected float MoveSpeed = .05f; // All Enemies to have MoveSpeed
 
-    protected abstract void OnHit();
-
+    // Abstract = incomplete, never defined
+    protected abstract void OnHit(); // undefined method that only child objects can define
+    // protected allows child classes access to variables and method, but not only classes
     protected Rigidbody RB { get; private set; }
 
     private void Awake()
@@ -24,8 +40,11 @@ public abstract class EnemyBase : MonoBehaviour
     {
         Move();
     }
+    // child classes have the ability to run Move(), but other classes do not
+    // Base Inheritance Example
     protected virtual void Move()
     {
+        // Allows enemies to move in a straight forward but can be modified to specifc child classes
         Vector3 moveDelta = transform.forward * MoveSpeed;
         RB.MovePosition(RB.position + moveDelta);
     }
@@ -44,7 +63,8 @@ public abstract class EnemyBase : MonoBehaviour
             }
         }
     }
-
+    // ANYTHING can call Kill() function. 
+    // Want the child class to modify when a enemy dies that could spawn power ups or spawn more enemies.
     public virtual void Kill()
     {
         AudioHelper.PlayClip2D(_deathSound, 1, .1f);
