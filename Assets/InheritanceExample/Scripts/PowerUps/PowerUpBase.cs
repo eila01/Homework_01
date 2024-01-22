a82e69b69c79b3cf717cd8dfb541c45a8de2b9d9
Eila <eilaboldt01@gmail.com>
2024-01-22 11:33:20 -0600

    Final update to Power Up
    Charger spawn a rapid fire power up that will last 2 seconds.

--- Assets/InheritanceExample/Scripts/PowerUps/PowerUpBase.cs
+++ Assets/InheritanceExample/Scripts/PowerUps/PowerUpBase.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public abstract class PowerUpBase : MonoBehaviour
 {
-    //[SerializeField] private int _health = 3;
-    //[Header("FX")]
-    //[SerializeField] private AudioClip _deathSound;
-    //[SerializeField] private AudioClip _hitSound;
-  //  [SerializeField] protected float MoveSpeed = .05f;
+    
     [SerializeField] protected float PowerUpDuration = .05f;
 
     protected abstract void OnHit();
@@ -19,32 +15,23 @@ public abstract class PowerUpBase : MonoBehaviour
 
     private void Awake()
     {
-        //RB = GetComponent<Rigidbody>();
+       
         Col = GetComponent<Collider>();
     }
 
-    private void FixedUpdate()
-    {
-        //Move();
-    }
     
-    //protected virtual void Move()
-    //{
-    //    Vector3 moveDelta = transform.forward * MoveSpeed;
-    //    RB.MovePosition(RB.position + moveDelta);
-    //}
+    
+    
 
     private void OnTriggerEnter(Collider other)
     {
         Projectile projectile = other.GetComponent<Projectile>();
         if (projectile != null)
         {
-            PowerUpDuration -= 0.01f;
-            //_health -= 1;
-           // AudioHelper.PlayClip2D(_hitSound, 1, .1f);
+            
             OnHit();
-            PowerUp();
-            Col.enabled = false;
+            
+           // Col.enabled = false;
             transform.Find("Art").gameObject.SetActive(false);
             if (PowerUpDuration <= 0)
             {
@@ -55,7 +42,7 @@ public abstract class PowerUpBase : MonoBehaviour
     
     public virtual void PowerDown()
     {
-       // AudioHelper.PlayClip2D(_deathSound, 1, .1f);
+       
         gameObject.SetActive(false);
     }
 }
--- Assets/InheritanceExample/Scripts/PowerUps/RapidFire.cs
+++ Assets/InheritanceExample/Scripts/PowerUps/RapidFire.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class RapidFire : PowerUpBase
 {
-    [SerializeField] private TurretController turrentController;
-    // private float _cooldown = TurretController.FireCooldown;
+    private TurretController turrentController;
+    
     private void Awake()
     {
-        if (!turrentController) turrentController = GetComponent<TurretController>();
+        if (!turrentController) turrentController = FindObjectOfType<TurretController>();
+        PowerUpDuration = 2f;
     }
     protected override void OnHit()
     {
-
+       
+        PowerUp();
     }
     protected override void PowerUp()
     {
+        Debug.Log("Cooldown Lowered");
         turrentController.FireCooldown = .25f;
+        StartCoroutine(PowerUpTimer());
+        
+        
+       
+    }
+    IEnumerator PowerUpTimer()
+    {
+
+        yield return new WaitForSeconds(PowerUpDuration);
+        RevertCooldown();
+    }
+    void RevertCooldown()
+    {
+        turrentController.FireCooldown = .5f;
+        Debug.Log("Cooldown Reverted");
     }
 }
